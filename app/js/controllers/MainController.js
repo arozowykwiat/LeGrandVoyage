fabed17cb2657871cee71a3530c7a29ac3afff33
arozow <arozowywiat@gmail.com>
2017-12-21 10:57:43 +0100

    sauv22: ajout des poèmes dans le controlleur

--- app/js/controllers/MainController.js
+++ app/js/controllers/MainController.js
@@ -196,6 +196,265 @@ var poeme=[
 		strophe:"<p>Les poissons sont bien plus éloquents<br/>que la passion reniée.</p>",
 		stropheGr:"<p>Τα ψάρια μού μιλάνε πιο καλά<br/>απ’ την αγάπη πούχεις αρνηθεί.</p>"
 
+	},
+	{
+		idPoeme:3,
+		idStrophe:1,
+		titre:"Dense et profond… ⎠",
+		titreGr:"⎝ Βαθύς και πυκνός..."
+	},
+	{
+		idPoeme:4,
+		idStrophe:1,
+		titre:"Appareillage ⎠",
+		titreGr:"⎝ Απόπλους",
+		strophe:"<p>Reste maintenant dans ta médiocrité,<br/>sans étincelle dans ton froid hivernal.<br/>Ton étreinte était douce, mais en moi<br/>elle n’a pas laissé de trace.<br/></p>",
+		stropheGr:"<p>Σ’ αφήνω πια στη μετριότητά σου,<br/>τη χειμωνιά σου άλλο δεν πυροδοτώ.<br/>Γλυκό ήτανε τ’ άγγιγμά σου, όμως<br/>στο χώμα μου δεν άφησε καρπό.</p>"
+
+	},
+	{
+		idPoeme:4,
+		idStrophe:2,
+		strophe:"<p>Adieu, que tes jours soient tranquilles<br/>et si le bonheur te blesse,<br/>vis la promesse d’un moment<br/>d’enivrante plénitude.</p>",
+		stropheGr:"<p>Αντίο, νάν’ οι μέρες σου ήσυχες,<br/>κι αφού η ευτυχία σε πονεί,<br/>ζήσε με την υπόσχεση πως θάρθει<br/>πληρότητας μεθυστική στιγμή.</p>"
+
+	},
+	{
+		idPoeme:4,
+		idStrophe:3,
+		strophe:"<p>Tu n’étais pas fait pour le goût salé<br/>qui brûle l’âme immature.<br/>Comment t’abreuver à l’eau cristalline<br/>de source sauvage ?</p>",
+		stropheGr:"<p>Δεν ήτανε για σένα η αλμυρή<br/>γεύση που τσούζει την άωρη ψυχή,<br/>πώς ν’ αγαπήσεις το κρυστάλλινο<br/>νερό από την άγρια την πηγή;</p>"
+	},
+	{
+		idPoeme:4,
+		idStrophe:4,
+		strophe:"<p>Je m’en vais, moi, vers les instants écarlates<br/>de l’abîme obscur.<br/></p>",
+		stropheGr:"<p>Πηγαίνω για τις άλυκες στιγμές<br/>που συναντούμε στο σκοτεινό βυθό.</p>"
+
+	},
+	{
+		idPoeme:5,
+		idStrophe:1,
+		titre:"J’ai la nostalgie… ⎠",
+		titreGr:"⎝ Νοστάλγησα…",
+		strophe:"<p>… du dernier reflet, oblique,<br/>sur la neige violette qui brille.</p>",
+		stropheGr:"<p>… την τελευταία αναλαμπή, πλάγια,<br/>στο μενεξεδί, αυτόφωτο χιόνι.</p>"
+
+	},
+	{
+		idPoeme:5,
+		idStrophe:2,
+		strophe:"<p>J’ai la nostalgie des espoirs forts<br/>et du temps qui ne regrette<br/>rien.</p>",
+		stropheGr:"<p>Nοστάλγησα τη δύναμη της ελπίδας<br/>και τον καιρό που για τίποτε δεν<br/>μετανιώνει.</p>"
+
+	},
+	{
+		idPoeme:6,
+		idStrophe:1,
+		titre:"Acherusia ⎠",
+		titreGr:"⎝ Αχερουσία",
+		strophe:"<p>Pourquoi pensent-ils autant à nous, les morts,<br/>Et pourquoi pensons-nous tant aux morts ?<br/>Nous comptons les jours en prêtant l’oreille<br>à la fin qui approche.</p>",
+		stropheGr:"<p>Πώς και μας μελετάνε τόσο οι νεκροί<br/>και βγήκαν να λιαστούν πάνω στο χιόνι;<br/>Πώς έτσι μελετάμε τόσο τους νεκρούς;<br/>Mετράμε μέρες κι αφουγκραζόμαστε<br/>το τέλος που σιμώνει.</p>"
+
+	},
+	{
+		idPoeme:6,
+		idStrophe:2,
+		strophe:"<p>Pourquoi réclament-ils toujours plus, les morts ?<br/>Ils engendrent de nouveaux délais<br/>sur ce qui semblait à jamais reporté :<br/>serait-ce la peur qui enfante<br/>chacun de nos désirs ?</p>",
+		stropheGr:"<p>Πώς όλο κάτι ακόμη μας γυρεύουν<br/>οι νεκροί; Kάτι πούχε παντοτινά<br/>αναβληθεί και πάλι προσμονή γεννά:<br/>μην είν’ ο φόβος, που κάθε μας λαχτάρα<br/>εγκυμονεί;</p>"
+	},
+	{
+		idPoeme:6,
+		idStrophe:3,
+		strophe:"<p>Qu’il est difficile de franchir,<br>la ligne imperceptible et ténue<br/>qui nous sépare des voix des vivants,<br/>et comme elles l’appellent de leurs voeux,<br/>le poids des morts qui les tire…</p>",
+		stropheGr:"<p>Δύσκολα κανείς διαβαίνει τη λεπτή,<br/>αδιόρατη γραμμή που μας χωρίζει<br/>απ’ τις φωνές των ζωντανών και πώς ποθούν<br/>το βάρος των νεκρών που τους ξεσέρνει…<br/>Aπό τη μια στην άλλην</p>"
+	},
+	{
+		idPoeme:6,
+		idStrophe:4,
+		strophe:"<p>D’une rive à l’autre, à contre-courant,<br/>ils échouent dans le fleuve du Léthé ;<br/>vol des papillons violets, ivresse,<br/>chant des rossignols. Qu’elle est ténue<br/>la ligne quand s’évente<br>la renaissance des peines dissimulées.</p>",
+		stropheGr:"<p>όχθη ποταμός λήθης κόντρα ρέμα·<br/>χοροπηδούν μαβιές φαρφάλες, μέθη,<br/>κελάηδισμα των αηδονιών. Τι λεπτή<br/>είν’ η γραμμή σαν ξεθυμαίνει ανάσταση<br/>κρυφών καημών.</p>"
+
+	},
+	{
+		idPoeme:7,
+		idStrophe:1,
+		titre:"Aspalathe ⎠",
+		titreGr:"⎝ Ασπάλαθος",
+		strophe:"<p>Prépare-toi à voler bas dans le brouillard ;<br/>là où méditent les âmes, où elles tâtent les traces,<br/>se figurent les scènes de la vie, calculent les rouages<br/>de l’infini. Ne demande pas…</p>",
+		stropheGr:"<p>Eτοιμάσου για πτήσεις χαμηλές μες στην ομίχλη·<br/>εκεί όπου στοχάζονται οι ψυχές· όπου τα ίχνη<br/>ψηλαφούν, αναθυμούνται τις σκηνές του βίου και<br/>προσμετρούν τα κίνητρα του απείρου. Μη ρωτήσεις…<br/></p>"
+
+	},
+	{
+		idPoeme:7,
+		idStrophe:2,
+		strophe:"<p>… si en sortant des nues elles conservent l’énergie,<br/>si elles interviennent dans les plans du destin, compensent<br/>peines de l’existence, erreurs de songes et d’actions.<br/>Passe doucement, les feux éteints, au ralenti,</p>",
+		stropheGr:"<p>… αν διατηρούν ενέργεια καθώς βγαίνουν απ’ τα νέφη,<br/>αν παρεμβαίνουν στα σχέδια του πεπρωμένου. Αν<br/>συμψηφίζουν άλγη της ζωής, σφάλματα πράξεων<br/>κι ονείρων. Να περάσεις σιγά, με τα φώτα σβηστά,</p>"
+	},
+	{
+		idPoeme:7,
+		idStrophe:3,
+		strophe:"<p>ne trouble pas leur réflexion ; la moindre fissure<br/>menace leur ouvrage réparateur. Garde à l’esprit<br/>le relief des monuments et des monts.<br/>Mais n’hésite pas de passer, qu’elles te sentent.</p>",
+		stropheGr:"<p>ραλαντί. Μ’ ανάγλυφο στο νου το χάρτη ορέων,<br/>μνημείων, μην ταράξεις το διαλογισμό των ψυχών:<br/>μια ανεπαίσθητη ρωγμή απειλεί το έργο τους<br/>το διορθωτικό. Να περάσεις όμως από εκεί,</p>"
+
+	},
+	{
+		idPoeme:7,
+		idStrophe:4,
+		strophe:"<p>Ton vol balise leurs ténèbres. N’aie pas peur ;<br/>tout ce qui est blanc n’est pas sommet.<br/>Ce sont peut-être des carrières, des salins.<br/>Et au-dessus de la côte dentelée de Split,</p>",
+		stropheGr:"<p>να σε νιώθουν. Τα ερέβη τους η δική σου πτήση<br/>σηματοδοτεί. Και μη φοβηθείς· κάθε τι λευκό<br/>δεν είναι κορυφή. Eίναι ίσως λατομεία, αλυκές.</br/>Kαι πάνω από την κεντημένη την ακτή του Σπλιτ,</p>"
+
+	},
+	{
+		idPoeme:7,
+		idStrophe:5,
+		strophe:"<p>où un cétacé géant barre une mer fermée,<br/>son ventre grand ouvert expose les monstres<br/>qu’il a avalés, et le sang noir pollue la côte,<br/>n’aie pas peur ; tiens bien le timon dans la brume ;</p>",
+		stropheGr:"<p>όπου γιγάντιο κήτος φράζει θάλασσα κλειστή<br/>κι η ορθάνοιχτη κοιλιά του προβάλλει τα τέρατα<br/>που έχει καταπιεί και σκούρο αίμα μολύνει<br/>την ακτή, μη φοβηθείς· τιμόνι γερά να κρατάς,</p>"
+
+	},
+	{
+		idPoeme:7,
+		idStrophe:6,
+		strophe:"<p>fixe la lumière d’un regard intérieur ;<br/>et prononce à cet instant précis ta malédiction :<br/>que soient engloutis dans leur propre salive<br/>les usurpateurs de gloires étrangères ;</p>",
+		stropheGr:"<p>στην αχλύ· εστίασε το βλέμμα νοερά στο φως<br/>και τη στιγμή εκείνη ακριβώς, να καταραστείς:<br/>μέσα στο ίδιο τους το σάλιο να καταποντιστούν<br/>όσοι δολίως ξένη δόξα αποκτούν· φολίδες</p>"
+
+	},
+		{
+		idPoeme:7,
+		idStrophe:7,
+		strophe:"<p>que des squames gravent la peau de qui a nui<br/>sans raison ; que ceux qui ont lésé soient punis.<br/>Mais épargne les innocents. Concentre-toi :<br/>parfois l’instant dure et, le temps de traverser,</p>",
+		stropheGr:"<p>να φυτρώσουνε σκληρές στο δέρμα όσων βλάψαν<br/>αναιτίως· κι όσοι αδίκησαν, να τιμωρηθούν.<br/>Να εξαιρέσεις όμως τους αθώους. Συγκεντρώσου:<br/>κάποτε μια στιγμή διαρκεί κι ωσότου να διαβείς</p>"
+
+	},
+	{
+		idPoeme:7,
+		idStrophe:8,
+		strophe:"<p>les clones de l’infamie se multiplient.<br/>Plein pot vers le soleil, alors, mémoire<br/>de nourrisson, peau de serpent, génie de papillon :<br/>ton circuit des mers unit les continents.</p>",
+		stropheGr:"<p>πολλαπλασιάζονται της ατιμίας οι κλώνοι.<br/>Έπειτα πάρε ύψος και πάνω από τα νέφη,<br/>μηχανές φουλ στον ήλιο, με μνήμη νηπίου, με νου<br/>πεταλούδας, με δέρμα φιδιού, κάνε το γύρο<br/>του ωκεανού, τις στεριές να ενώσεις.</p>"
+
+	},
+	{
+		idPoeme:8,
+		idStrophe:1,
+		titre:"Il y a de la sérénité… ⎠",
+		titreGr:"⎝ Υπάρχει γαλήνη...",
+		strophe:"<p>… dans ces nues<br/>quand elles s’ouvrent brusquement au soleil aveuglant<br/>qui va se coucher</p>",
+		stropheGr:"<p>… σ’ αυτά τα νέφη<br/>καθώς ανοίγουν ξαφνικά στον ήλιο,<br/>τον εκτυφλωτικό, που πάει να δύσει</p>"
+
+	},
+	{
+		idPoeme:8,
+		idStrophe:2,
+		strophe:"<p>et puis lourdes s’amoncellent de nouveau<br/>en s’approchant grises, menaçantes,<br/>avec le vent ?</p>",
+		stropheGr:"<p>κι ύστερα πάλι σωριάζονται βαριά,<br/>γίνονται γκρίζα και απειλητικά,<br/>καθώς με τον άνεμο πλησιάζουν;</p>"
+
+	},
+	{
+		idPoeme:8,
+		idStrophe:3,
+		strophe:"<p>Il y a de la sérénité dans cette mer<br/>qui rit quand elle t’emporte et te perd,<br/>qui te touche en pleurant ? Vraiment ?</p>",
+		stropheGr:"<p>Yπάρχει γαλήνη σ’ αυτή τη θάλασσα,<br/>που όταν γελά σε παίρνει να χάνεσαι<br/>και κλαίει όταν σ’ ακουμπά; Yπάρχει;</p>"
+
+	},
+	{
+		idPoeme:8,
+		idStrophe:4,
+		strophe:"<p>Et pourquoi vouloir la sérénité<br/>quand soudain les nues s’embrasent<br/>et commence le coucher du soleil,</p>",
+		stropheGr:"<p>Kαι γιατί να θέλεις τη γαλήνη,<br/>σαν άξαφνα τα σύννεφα φλέγονται<br/>και δραματική, σπαρακτική δύση</p>"
+
+	},
+		{
+		idPoeme:8,
+		idStrophe:5,
+		strophe:"<p>dramatique, déchirant ? Le silence<br/>succède à l’épuisement. L’épuisement<br/>vient toujours après le combat,</p>",
+		stropheGr:"<p>του ήλιου αρχινά; Σιγή ακολουθεί<br/>την εξάντληση. Και η εξάντληση<br/>έρχεται πάντα ύστερα από τη μάχη,</p>"
+
+	},
+		{
+		idPoeme:8,
+		idStrophe:6,
+		strophe:"<p>rassemblant des corps démembrés,<br/>des trésors pillés, essuyant<br/>le sang sur le pont.</p>",
+		stropheGr:"<p>μαζεύοντας διαμελισμένα κορμιά,<br/>συλλημένους θησαυρούς, σφογγίζοντας<br/>τα αίματα πάνω στο κατάστρωμα.</p>"
+
+	},
+		{
+		idPoeme:8,
+		idStrophe:7,
+		strophe:"<p>Le capitaine est alors général.<br/>Il juge les traîtres et impose le silence.</p>",
+		stropheGr:"<p>Ο καπετάνιος τότε είναι στρατηγός.<br/>Κρίνει τους προδότες κι επιβάλλει σιωπή.<br/>Γι’ αυτό φοβάμαι τη γαλήνη: είναι</p>"
+
+	},
+		{
+		idPoeme:8,
+		idStrophe:8,
+		strophe:"<p>Voilà pourquoi j’ai peur de la sérénité :<br/>c’est l’heure du châtiment.</p>",
+		stropheGr:"<p>ώρα τιμωρίας.</p>"
+
+	},
+		{
+		idPoeme:9,
+		idStrophe:1,
+		titre:"Barcarolle ⎠",
+		titreGr:"⎝ Βαρκαρόλα",
+
+	},
+		{
+		idPoeme:10,
+		idStrophe:1,
+		titre:"Les migrateurs ⎠",
+		titreGr:"⎝ Τ’ αποδημητικά",
+		strophe:"<p>Les revoici, les oiseaux migrateurs qui chantent.<br/>Qu’ont-ils encore inventé pour affoler les branches<br/>et se faire entendre jusqu’à l’autre côté du boulevard<br/>sans répit ?</p>",
+		stropheGr:"<p>Να πάλι τραγουδούν τ’ αποδημητικά πουλιά.<br/>Ποιος ξέρει τι σκαρώνουν και τρελλαίνουν τα κλαδιά·<br/>κι ακούγονται ως την άλλη πλευρά της λεωφόρου<br/>ασταμάτητα.</p>"
+
+	},
+	{
+		idPoeme:10,
+		idStrophe:2,
+		strophe:"<p>Le revoilà, inquiet, le crépuscule de l’automne.<br/>Certains frappent le poing sur le fer, d’autres mènent<br/>un rude combat contre l’usure, se tourmentent<br/>et attendent.</p>",
+		stropheGr:"<p>Να πάλι, ανήσυχο δειλινό φθινοπώρου.<br/>Κάποιοι πάνω στο σίδερο χτυπάνε τη γροθιά,<br/>κάποιοι παλεύουνε σκληρά με τη φθορά, ανησυχούν<br/>και περιμένουν.</p>"
+
+	}
+	,
+	{
+		idPoeme:10,
+		idStrophe:3,
+		strophe:"<p>Ils sont intransigeants, les migrateurs. Et nul ne sait<br/>ce qu’ils trament. Aujourd’hui, la lumière peine<br/>à toucher les galets. S’élève la nostalgie<br/>du sable tiède.</p>",
+		stropheGr:"<p>Πάντα είν’ έτσι απόλυτα τ’ αποδημητικά.<br/>Kανείς δεν ξέρει τι σκαρώνουν. Δύσκολα τώρα<br/>το φως πάνω στα βότσαλα ακουμπά. Νοσταλγία<br/>χτίζεται της χλιαρής της άμμου.Tα λιοπύρια</p>"
+
+	}
+	,
+	{
+		idPoeme:10,
+		idStrophe:4,
+		strophe:"<p>Les fortes chaleurs ont facilement glissé<br/>sur les corps en sueur ; et j’ai de nouveau déposé doucement<br/>un voeu oublié déjà, un voeu sur les galets du sable.</p>",
+		stropheGr:"<p>ξετυλίχτηκαν γοργά στα ιδρωμένα τα κορμιά<br/>κι άφησα πάλι την ευχή στα σιγαλά,<br/>λησμονημένη κιόλας,<br/>ευχή πάνω στα βότσαλα της άμμου.</p>"
+
+	}
+	,
+	{
+		idPoeme:11,
+		idStrophe:1,
+		titre:"Retour ⎠",
+		titreGr:"⎝ Επιστροφή",
+		strophe:"<p>Là où l’homme<br/>n’est qu’une ombre<br/>et la lune une lumière,<br/>quand la forteresse se perd<br/>dans le brouillard des eaux,<br/>reconvertis le serpent en or<br/>jusqu’à ce que le bateau rallie<br/>la géométrie de notre destin.</p>",
+		stropheGr:"<p>Eκεί όπου ο άνθρωπος<br/>και το φεγγάρι ένα φως,<br/>όταν το φρούριο χάνεται<br/>στων υδάτων την ομίχλη,<br/>κάνε πάλι το φίδι χρυσάφι,<br/>ώσπου να δέσει το καράβι<br/>στη γεωμετρία της μοίρας μας.</p>"
+
+	}
+	,
+	{
+		idPoeme:11,
+		idStrophe:2,
+		strophe:"<p>À l’azimut des statues<br/>et des anges emmenez-moi,<br/>créatures de saintes volontés.<br/>Le premier astre qui rit<br/>sur le clocher rose mesure<br/>le temps sur les mers<br/>et par l’ombre le poisson est pris.<br/>Là où l’homme</p>",
+		stropheGr:"<p>Στο αζιμούθιο των αγαλμάτων<br/>και των αγγέλων με οδηγείτε,<br/>πλάσματα βουλήσεων Aγίων.<br/>Tο πρώτο αστέρι που γελά<br/>στο ρόδινο καμπαναριό μετρά<br/>το χρόνο πάνω στα πέλαγα<br/>και πιάνετ’ από τη σκιά το ψάρι.<br/>Eκεί όπου ο άνθρωπος</p>"
+
+	}
+	,
+	{
+		idPoeme:11,
+		idStrophe:3,
+		strophe:"<p>n’est qu’ombre sur le rocher<br/>et la lune lumière qui scintille,<br/>là où nage le poisson<br/>et la mer étend un coucher<br/>de velours dans le brouillard de l’hiver,<br/>fais ton miracle, saint patron,<br/>amarre à la forteresse doucement<br/>le bateau éprouvé.</p>",
+		stropheGr:"<p>είναι μοναχά σκιά στο βράχο και<br/>το φεγγάρι, φως που τρεμοπαίζει,<br/>εκεί που κολυμπά το ψάρι<br/>κι η θάλασσα απλώνει δειλινό<br/>βελούδο στη χειμωνιάτικη ομίχλη,<br/>Άγιε μου κάνε το θαύμα σου,<br/>στο φρούριο ν’ αράξει σιγαλά<br/>το πολύπαθο καράβι.</p>"
+
 	}
 	];
 var photo=[
